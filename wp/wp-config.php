9e25916cbbc77f7bc70ba04a9c021bc31250d76a
morekid <morekid@morekids-iMac.station>
2020-03-29 13:22:25 +0200

    Direct plugin management in admin (no ftp auth)

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -88,3 +88,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+/** Allows plugin CRUD without FTP auth */
+define( 'FS_METHOD', 'direct' );
